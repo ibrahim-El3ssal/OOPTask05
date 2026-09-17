4a24d7193f66da90243924eb38dd8006c9d142e1
ibrahim-El3ssal <ibrahimelassal662@gmail.com>
2026-09-17 11:31:49 +0300

    Add project files.

---
+++ OOP01Task05/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP01Task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
